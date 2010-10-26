843677805fb0ff2d5d512bb601dbf07fa26bb370
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2010-10-26 16:13:22 +0200

    Import code

---
+++ PaintingPro 1/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow 
+    Public Event Arrêter(ByVal sender As Object, ByVal e As System.EventArgs)
+
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        Close()
+        End
+    End Sub
+End Class
